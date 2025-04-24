6fd22fe4f4d5e2ed7a6d2397cac50b8dd30647e2
Jeung9711 <134358261+Jeung9711@users.noreply.github.com>
2025-04-25 00:57:22 +0900

    20250424 study and project dusty

---
+++ dustycity/dustycity/src/main/java/com/example/dustycity/DustycityApplication.java
@@ -0,0 +1,13 @@
+package com.example.dustycity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DustycityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DustycityApplication.class, args);
+	}
+
+}
---
+++ dustycity/dustycity/src/test/java/com/example/dustycity/DustycityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dustycity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustycityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
